961e6d561c06e8c955eb7378a1e92f311304b335
Vidish <joglekar.vidish9@gmail.com>
2016-06-01 09:16:51 +1000

    Added removeProperty function and hover effect to show add and remove buttons when hovered over the property listing

--- app/controller/controller.js
+++ app/controller/controller.js
@@ -3,24 +3,30 @@ angular.module('PropertySaver').controller('PropertyController', function Proper
     $scope.urlProperties = "properties.json";
     $scope.urlSavedProperties = "saved-properties.json";
     var arrProperties, arrSavedProperties = [];
+
     
+    // Get properties from the properties.json file
     $http.get($scope.urlProperties).success(function(data) {
         
         arrProperties = $scope.properties = data;
         
     });
     
+    
+    // Get saved properties from the saved-properties.json file
     $http.get($scope.urlSavedProperties).success(function(data) {
        
         arrSavedProperties = $scope.savedProperties = data; 
          
     });
     
+    
+    // Add property function
     $scope.addProperty = function(num){
         
         var i, flag = 0;
         
-        for(i=0; i<arrSavedProperties.length; i++){
+        for(i = 0; i < arrSavedProperties.length; i++){
             if(arrSavedProperties[i].id == num){
                 flag = 1;
             }
@@ -44,4 +50,16 @@ angular.module('PropertySaver').controller('PropertyController', function Proper
             alert("Property has already been saved!");
         }
     }
+    
+    
+    // Remove property function
+    $scope.removeProperty = function(num){
+        var j;
+        for(j = 0; j < arrSavedProperties.length; j++){
+            if(num == arrSavedProperties[j].id){
+                arrSavedProperties.splice(j, 1);
+            }
+        }    
+    }   
+    
 });
